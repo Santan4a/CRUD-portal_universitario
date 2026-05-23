50070ffaa54f9554f9f6f83d3fba5a3765749b6c
João Santana <Joutaa32@gmail.com>
2026-05-23 02:24:53 -0300

    feat: add shell script for automation

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#bash run.sh, cod para rodar o script run.
+
+if [ -f ".venv/Scripts/python.exe" ]; then
+    VENV_PYTHON=".venv/Scripts/python.exe"
+
+elif [ -f ".venv/bin/python" ]; then
+    VENV_PYTHON=".venv/bin/python"
+
+else
+    echo "Ambiente virtual não encontrado"
+    exit 1
+fi
+
+$VENV_PYTHON manage.py migrate
+$VENV_PYTHON manage.py runserver
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#bash setup.sh, cod para rodar o script do setup.
+
+if command -v python &> /dev/null; then
+    PYTHON=python
+
+elif command -v python3 &> /dev/null; then
+    PYTHON=python3
+
+else
+    echo "Python não encontrado"
+    exit 1
+fi
+
+if [ ! -d ".venv" ]; then
+    echo "Ambiente virtual não encontrado! Criando ambiente virtual..."
+    
+    if ! $PYTHON -m venv .venv; then
+        echo "Erro ao criar ambiente virtual"
+        exit 1
+    fi
+fi
+
+$PYTHON -m pip install -r requirements.txt
