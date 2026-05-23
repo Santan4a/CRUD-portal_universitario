d929667dd147ab8209dd43d6552255f195bdd9b3
Erlon Matheus <erlonsttpro2018@gmail.com>
2026-05-23 02:54:47 -0300

    feat: filter subjects by selected student

--- notas/forms.py
+++ notas/forms.py
@@ -1,11 +1,50 @@
 from django import forms
+
+from alunos.models import Aluno
+from disciplinas.models import Disciplina
 from .models import Nota
+
 
 class NotaForm(forms.ModelForm):
 
     class Meta:
         model = Nota
         fields = ['aluno', 'disciplina', 'nota1', 'nota2']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['disciplina'].queryset = Disciplina.objects.none()
+        self.fields['disciplina'].empty_label = 'Selecione um aluno primeiro'
+
+        aluno = self.obter_aluno_selecionado()
+
+        if aluno:
+            self.fields['disciplina'].queryset = aluno.disciplinas.order_by(
+                'nome',
+                'codigo'
+            )
+            self.fields['disciplina'].empty_label = 'Selecione uma disciplina'
+
+    def obter_aluno_selecionado(self):
+        aluno_id = None
+        aluno_inicial = self.initial.get('aluno')
+
+        if self.is_bound:
+            aluno_id = self.data.get(self.add_prefix('aluno'))
+        elif self.instance and self.instance.pk:
+            aluno_id = self.instance.aluno_id
+        elif isinstance(aluno_inicial, Aluno):
+            return aluno_inicial
+        elif aluno_inicial:
+            aluno_id = aluno_inicial
+
+        if not aluno_id:
+            return None
+
+        try:
+            return Aluno.objects.prefetch_related('disciplinas').get(pk=aluno_id)
+        except (Aluno.DoesNotExist, TypeError, ValueError):
+            return None
 
     def clean(self):
         cleaned_data = super().clean()
--- notas/tests.py
+++ notas/tests.py
@@ -5,6 +5,7 @@ from django.urls import reverse
 from alunos.models import Aluno
 from disciplinas.models import Disciplina
 from users.models import Profile
+from .forms import NotaForm
 from .models import Nota
 
 
@@ -53,6 +54,7 @@ class NotaAccessTests(TestCase):
         )
         outro_aluno = Aluno.objects.create(nome='Bruno Lima', matricula='N103')
         disciplina = Disciplina.objects.create(nome='Matematica', codigo='MAT102')
+        aluno.disciplinas.add(disciplina)
         Nota.objects.create(aluno=aluno, disciplina=disciplina, nota1=8, nota2=7)
         Nota.objects.create(aluno=outro_aluno, disciplina=disciplina, nota1=4, nota2=5)
         self.client.force_login(aluno_user)
@@ -71,3 +73,148 @@ class NotaAccessTests(TestCase):
         response = self.client.get(reverse('criar_nota'))
 
         self.assertEqual(response.status_code, 403)
+
+    def test_gestao_can_create_nota(self):
+        gestao_user = User.objects.create_user(
+            username='gestao_cria_nota',
+            password='gestao12345'
+        )
+        Profile.objects.create(user=gestao_user, role='gestao')
+        aluno = Aluno.objects.create(nome='Ana Silva', matricula='N104')
+        disciplina = Disciplina.objects.create(nome='Matematica', codigo='MAT103')
+        aluno.disciplinas.add(disciplina)
+        self.client.force_login(gestao_user)
+
+        response = self.client.post(
+            reverse('criar_nota'),
+            {
+                'aluno': aluno.id,
+                'disciplina': disciplina.id,
+                'nota1': 9,
+                'nota2': 8,
+            },
+        )
+
+        self.assertRedirects(response, reverse('lista_notas'))
+        self.assertTrue(Nota.objects.filter(
+            aluno=aluno,
+            disciplina=disciplina,
+            nota1=9,
+            nota2=8,
+        ).exists())
+
+    def test_gestao_can_edit_nota(self):
+        gestao_user = User.objects.create_user(
+            username='gestao_edita_nota',
+            password='gestao12345'
+        )
+        Profile.objects.create(user=gestao_user, role='gestao')
+        aluno = Aluno.objects.create(nome='Ana Silva', matricula='N105')
+        disciplina = Disciplina.objects.create(nome='Matematica', codigo='MAT104')
+        aluno.disciplinas.add(disciplina)
+        nota = Nota.objects.create(
+            aluno=aluno,
+            disciplina=disciplina,
+            nota1=6,
+            nota2=7,
+        )
+        self.client.force_login(gestao_user)
+
+        response = self.client.post(
+            reverse('editar_nota', args=[nota.id]),
+            {
+                'aluno': aluno.id,
+                'disciplina': disciplina.id,
+                'nota1': 8,
+                'nota2': 9,
+            },
+        )
+
+        nota.refresh_from_db()
+        self.assertRedirects(response, reverse('lista_notas'))
+        self.assertEqual(nota.nota1, 8)
+        self.assertEqual(nota.nota2, 9)
+
+    def test_gestao_lista_notas_without_delete_action(self):
+        gestao_user = User.objects.create_user(
+            username='gestao_lista_nota',
+            password='gestao12345'
+        )
+        Profile.objects.create(user=gestao_user, role='gestao')
+        aluno = Aluno.objects.create(nome='Ana Silva', matricula='N106')
+        disciplina = Disciplina.objects.create(nome='Matematica', codigo='MAT105')
+        Nota.objects.create(aluno=aluno, disciplina=disciplina, nota1=8, nota2=7)
+        self.client.force_login(gestao_user)
+
+        response = self.client.get(reverse('lista_notas'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Nova Nota')
+        self.assertContains(response, 'Editar')
+        self.assertNotContains(response, 'Excluir')
+
+    def test_gestao_cannot_delete_nota(self):
+        gestao_user = User.objects.create_user(
+            username='gestao_nao_deleta_nota',
+            password='gestao12345'
+        )
+        Profile.objects.create(user=gestao_user, role='gestao')
+        aluno = Aluno.objects.create(nome='Ana Silva', matricula='N107')
+        disciplina = Disciplina.objects.create(nome='Matematica', codigo='MAT106')
+        nota = Nota.objects.create(
+            aluno=aluno,
+            disciplina=disciplina,
+            nota1=8,
+            nota2=7,
+        )
+        self.client.force_login(gestao_user)
+
+        response = self.client.post(reverse('deletar_nota', args=[nota.id]))
+
+        self.assertEqual(response.status_code, 403)
+        self.assertTrue(Nota.objects.filter(id=nota.id).exists())
+
+    def test_nota_form_starts_without_all_disciplines(self):
+        form = NotaForm()
+
+        self.assertFalse(form.fields['disciplina'].queryset.exists())
+
+    def test_nota_form_filters_disciplines_by_selected_student(self):
+        aluno = Aluno.objects.create(nome='Ana Silva', matricula='N108')
+        disciplina_do_aluno = Disciplina.objects.create(
+            nome='Matematica',
+            codigo='MAT107'
+        )
+        outra_disciplina = Disciplina.objects.create(
+            nome='Fisica',
+            codigo='FIS101'
+        )
+        aluno.disciplinas.add(disciplina_do_aluno)
+
+        form = NotaForm(data={'aluno': aluno.id})
+
+        self.assertIn(disciplina_do_aluno, form.fields['disciplina'].queryset)
+        self.assertNotIn(outra_disciplina, form.fields['disciplina'].queryset)
+
+    def test_nota_form_rejects_discipline_outside_student_course(self):
+        aluno = Aluno.objects.create(nome='Ana Silva', matricula='N109')
+        disciplina_do_aluno = Disciplina.objects.create(
+            nome='Matematica',
+            codigo='MAT108'
+        )
+        outra_disciplina = Disciplina.objects.create(
+            nome='Fisica',
+            codigo='FIS102'
+        )
+        aluno.disciplinas.add(disciplina_do_aluno)
+
+        form = NotaForm(data={
+            'aluno': aluno.id,
+            'disciplina': outra_disciplina.id,
+            'nota1': 8,
+            'nota2': 7,
+        })
+
+        self.assertFalse(form.is_valid())
+        self.assertIn('disciplina', form.errors)
+
