c4263426cb5c2263409e5f19ad476cdf65ec67c6
Guilherme Marinho <guilhermesilvamarinho88gmail.com>
2026-05-29 02:31:32 -0300

    feat: adiciona exportação de notas em PDF e Excel/professor

--- notas/urls.py
+++ notas/urls.py
@@ -6,4 +6,21 @@ urlpatterns = [
     path('nova/', views.criar_nota, name='criar_nota'),
     path('editar/<int:id>/', views.editar_nota, name='editar_nota'),
     path('deletar/<int:id>/', views.deletar_nota, name='deletar_nota'),
+    path(
+    'exportar/',
+    views.exportar_notas,
+    name='exportar_notas'
+),
+
+path(
+    'exportar/pdf/',
+    views.exportar_pdf,
+    name='exportar_pdf'
+),
+
+path(
+    'exportar/excel/',
+    views.exportar_excel,
+    name='exportar_excel'
+),
 ]
